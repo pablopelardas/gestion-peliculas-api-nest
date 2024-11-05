268c806b8f2d4bc7182bf8638d6ca6ce6454c474
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-11-05 18:15:34 -0300

    Chore: Nest generated auth and movies resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidation } from './config/joi-validation.config';
+import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
       validationSchema: JoiValidation,
-    })
+    }),
+    AuthModule,
+    MoviesModule
   ],
 })
 export class AppModule {}
